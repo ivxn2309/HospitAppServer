e6055e3941406524ea42b9bade9d009055509d5f
Maritza <maritzaesparza05@gmail.com>
2015-05-14 11:31:52 -0500

    Adición de ParserInterval.java a el paquete de utilerías

---
+++ src/java/multixsoft/hospitapp/utilities/Interval.java
@@ -0,0 +1,46 @@
+
+public class Interval {
+	private int start;
+	private int end;
+	
+	public Interval(int start, int end){
+		this.start = start;
+		this.end = end;
+	}
+	
+	public Interval(String cadenaIntervalos){
+		String[] hoursInterval = cadenaIntervalos.split("-");
+		if(hoursInterval.length > 0){
+			this.start = Integer.parseInt(hoursInterval[0]);
+			this.end = Integer.parseInt(hoursInterval[1]);
+		}
+	}
+	
+	public boolean isInsideInterval(int hour){
+		return ((hour >= this.start) && (hour <= this.end));
+	}
+	
+	public String breakIntervals(int hour){
+		String timeInterval = start+"-"+hour+" ";
+		if(!((hour+1) >= end)){
+			 timeInterval += (hour+1)+"-"+end;
+		}
+		return timeInterval;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}	
+}
