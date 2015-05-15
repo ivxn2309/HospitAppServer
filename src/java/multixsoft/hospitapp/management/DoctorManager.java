d19ffc2a50128e5ef8437e14fb578a2a57007cd5
AlexMtz <yamop.16@gmail.com>
2015-05-15 15:40:59 -0500

    metodo save new doctor agregado

--- src/java/multixsoft/hospitapp/management/DoctorManager.java
+++ src/java/multixsoft/hospitapp/management/DoctorManager.java
@@ -13,11 +13,14 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import multixsoft.hospitapp.webservice.AdapterRest;
+import org.json.simple.JSONArray;
 
 /**
  * REST Web Service
- *
- * @author YONY
+ * @author Yonathan Martínez
+ * @version 1.0
+ * 
  */
 @Path("doctormanager")
 public class DoctorManager {
@@ -30,12 +33,11 @@ public class DoctorManager {
      */
     public DoctorManager() {
     }
-
+    
     /**
      * Retrieves representation of an instance of multixsoft.hospitapp.management.DoctorManager
      * @return an instance of java.lang.String
-     */
-    @GET
+     *    @GET
     @Produces("application/json")
     public String getJson() {
         //TODO return proper representation object
@@ -46,9 +48,22 @@ public class DoctorManager {
      * PUT method for updating or creating an instance of DoctorManager
      * @param content representation for the resource
      * @return an HTTP response with content of the updated or created resource.
-     */
+     
     @PUT
     @Consumes("application/json")
     public void putJson(String content) {
+    }
+    * */
+    @PUT
+    @PathParam("/savenewdoctor")
+    @Produces ("aplication/json")
+    public String putSaveNewDoctor(
+        @QueryParam("username") String usrname){
+        AdapterRest adapter = new AdapterRest();
+        String path = "doctor/id=" + usrname;
+        JSONArray array = new (JSONArray) adapter.get(path);
+        
+        
+        return array.toJSONString();
     }
 }
